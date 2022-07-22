fa17a56220d4af7069eb7d8b4b385bfaefc8b8bc
monster anshu <hgunwant2312@gmail.com>
2022-07-22 19:11:03 +0530

    v1.0

--- src/api.ts
+++ src/api.ts
@@ -3,15 +3,23 @@ import { router as user_routes } from './Routes/user.routes';
 import { router as auth_routes } from './Routes/auth.routes';
 import { router as messages_routes } from './Routes/messages.routes';
 import { handle_error } from './Utils/handle_error';
-import cors from 'cors';
+import cors, { CorsOptions } from 'cors';
 export const api = Router();
 
-api.use(
-  cors({
-    origin: '*',
-    allowedHeaders: '*',
-  }),
-);
+const corsOption: CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: '*',
+  preflightContinue: false,
+};
+api.use(cors(corsOption));
 api.use('/auth', auth_routes);
 api.use('/user', user_routes);
 api.use('/chat', messages_routes);
