dc052bb974fde38296dd85570c56b07079a35db9
monster anshu <hgunwant2312@gmail.com>
2022-07-22 18:24:56 +0530

    v1.0

--- src/Utils/handle_error.ts
+++ src/Utils/handle_error.ts
@@ -1,11 +1,6 @@
 import { Request, Response, ErrorRequestHandler, NextFunction } from 'express';
 
-export const handle_error = (
-  error: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+export const handle_error: ErrorRequestHandler = (error, req, res, next) => {
   return res
     .status(500)
     .json({ success: false, error: error.message, mgs: 'Server Error' });
