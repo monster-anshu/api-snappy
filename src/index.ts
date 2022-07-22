9d22099be6d41a57b0d994085c351b4b2641a97a
monster anshu <hgunwant2312@gmail.com>
2022-07-22 16:15:10 +0530

    v1.0

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,22 @@ import { Server } from 'socket.io';
 import { connect } from './Database/mongo';
 import { api } from './api';
 import { remove_socket_id, set_socket_id } from './Utils/socket';
+import helmet from 'helmet';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // * Middleware
-app.use(morgan('common'));
-app.use(express.json());
 app.use(
   cors({
     origin: '*',
-    allowedHeaders: '*',
   }),
 );
+
+app.use(morgan('common'));
+app.use(express.json());
 app.use('/api', api);
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.send('ok');
