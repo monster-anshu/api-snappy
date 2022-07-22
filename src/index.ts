a32d8716ccf43e2cb10895de32fe8369615bfe30
monster anshu <hgunwant2312@gmail.com>
2022-07-22 19:14:43 +0530

    v1.0

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@ import cors, { CorsOptions } from 'cors';
 import dotenv from 'dotenv';
 import { Server } from 'socket.io';
 import { connect } from './Database/mongo';
-import { api } from './api';
 import { remove_socket_id, set_socket_id } from './Utils/socket';
+import { router as user_routes } from './Routes/user.routes';
+import { router as auth_routes } from './Routes/auth.routes';
+import { router as messages_routes } from './Routes/messages.routes';
+import { handle_error } from './Utils/handle_error';
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -28,10 +32,15 @@ app.use(cors(corsOption));
 
 app.use(morgan('common'));
 app.use(express.json());
-app.use('/api', api);
 app.get('/', (req, res) => {
   res.send('ok');
 });
+
+app.use(cors(corsOption));
+app.use('/api/auth', auth_routes);
+app.use('/api/user', user_routes);
+app.use('/api/chat', messages_routes);
+app.use(handle_error);
 
 const server = app.listen(PORT, () => {
   console.log('Server is running');
